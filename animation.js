12113bd8fa00315b6cc6790ac33e282c945326d5
Kone mohamed Lamine <mohamed14.kone@uvci.edu.ci>
2025-02-09 13:08:28 +0000

    mon site

---
+++ animation.js
@@ -0,0 +1,4 @@
+
+function affichermessage() {
+    alert("Merci de compter parmis nos visiteurs \n Pour plus d'informations sur moi , n'hésitez pas à me contacter \n C'est avec plaisir que je repondrais à toutes vos questions ")
+} ;
